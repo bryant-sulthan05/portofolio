d60b1bd3bdb69dc050778e8de5c14e1955d6aeb6
Bryant1356 <bryantv0.2.0@gmail.com>
2023-11-05 20:31:14 +0700

    Updates

---
+++ precache-manifest.681e076705042335f6ec3a96310a6e95.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "eca3be070f3d0661696b",
+    "url": "/portofolio/static/js/0.eca3be07.chunk.js"
+  },
+  {
+    "revision": "56657d844091bdae58b0",
+    "url": "/portofolio/static/js/1.56657d84.chunk.js"
+  },
+  {
+    "revision": "60170f1a9197b20433a8",
+    "url": "/portofolio/static/js/main.60170f1a.chunk.js"
+  },
+  {
+    "revision": "ae6dbeafb72f2af85985",
+    "url": "/portofolio/static/js/3.ae6dbeaf.chunk.js"
+  },
+  {
+    "revision": "4061270b51e0b64796c1",
+    "url": "/portofolio/static/js/4.4061270b.chunk.js"
+  },
+  {
+    "revision": "b6e294f2d071af98e8b9",
+    "url": "/portofolio/static/js/5.b6e294f2.chunk.js"
+  },
+  {
+    "revision": "62a13ca08ca5071c431b",
+    "url": "/portofolio/static/js/6.62a13ca0.chunk.js"
+  },
+  {
+    "revision": "c9c55fcc6e580c9e164e",
+    "url": "/portofolio/static/js/7.c9c55fcc.chunk.js"
+  },
+  {
+    "revision": "4cdb22778daaf40bb033",
+    "url": "/portofolio/static/css/8.9ed41780.chunk.css"
+  },
+  {
+    "revision": "4cdb22778daaf40bb033",
+    "url": "/portofolio/static/js/8.4cdb2277.chunk.js"
+  },
+  {
+    "revision": "28074beac7c781ea3b16",
+    "url": "/portofolio/static/js/9.28074bea.chunk.js"
+  },
+  {
+    "revision": "c01c96cbee6398a95b77",
+    "url": "/portofolio/static/js/runtime~main.c01c96cb.js"
+  },
+  {
+    "revision": "b7242574b0045a343111c29dc780c4c8",
+    "url": "/portofolio/static/media/pkl.b7242574.png"
+  },
+  {
+    "revision": "53b7b1944850a221354abda32ba4948c",
+    "url": "/portofolio/static/media/spp.53b7b194.png"
+  },
+  {
+    "revision": "943be19f8e078ea8a6092e2eca8a9e35",
+    "url": "/portofolio/static/media/evote.943be19f.png"
+  },
+  {
+    "revision": "4463e3ac1a5bf963023dbf91ee1dfe5f",
+    "url": "/portofolio/static/media/profile.4463e3ac.jpg"
+  },
+  {
+    "revision": "e0aae592b9362fce08d4cd3378c25cc7",
+    "url": "/portofolio/index.html"
+  }
+];
